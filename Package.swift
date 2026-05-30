2ed8d32825003fd5daf92652a0b9923a41fe8189
novalagung <hello@novalagung.com>
2026-05-30 15:44:04 +0700

    chore: explicitly set minimum supported version on SPM

--- Package.swift
+++ Package.swift
@@ -4,6 +4,12 @@ import PackageDescription
 
 let package = Package(
     name: "NVDate",
+    platforms: [
+        .iOS(.v12),
+        .macOS(.v10_13),
+        .tvOS(.v12),
+        .watchOS(.v4),
+    ],
     products: [
         .library(
             name: "NVDate",
